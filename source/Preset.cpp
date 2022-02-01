cd2ee3fc2707add6436925c376af8beb2c936238
Chikashi Miyama <chikashimiyama@gmail.com>
2022-02-01 22:29:17 +0100

    add workflow and installer

--- source/Preset.cpp
+++ source/Preset.cpp
@@ -10,7 +10,7 @@ namespace rp::curtis
         Preset{"come to daddy", -100.f, 25.f, 0, 3, 100, true, -1.f, 1.f, -1.f, 1.f, -0.5f, 0.5f, -0.5f, 0.5f, -80.f, 0.f},
         Preset{"convergence", -100.f, 100.f, 0, 0, 100, true, -3.f, 3.f, 0.f, 0.f, -1.f, 1.f, 0.f, 0.f, -70.f, 0.f},
         Preset{"toxic infant", -100.f, 10.f, 0, 0, 100, true, 11.0f, 13.0f, 11.0f, 13.0f, -0.1f, 0.1f, -0.1f, 0.1f, -10.f, 0.f},
-        Preset{"Granpa", -100.f, 10.f, 0, 0, 100, true, -13.0f, -11.0f, -13.0f, -11.0f, -0.1f, 0.1f, -0.05f, 0.05f, -10.f, 0.f},
+        Preset{"granpa", -100.f, 10.f, 0, 0, 100, true, -13.0f, -11.0f, -13.0f, -11.0f, -0.1f, 0.1f, -0.05f, 0.05f, -10.f, 0.f},
         Preset{"retro shooter", -100.f, 30.f, 0, 5, 100, true, 12.f, 24.f, -24.0f, -12.0f, -1.f, 1.f, -1.f, 1.f, -80.f, 0.f},
         Preset{"chatter", -100.f, 250.f, 0, 10, 100, true, -24.f, 24.f, 24.0f, 24.0f, -1.f, 1.f, -1.f, 1.f, -80.f, 0.f},
         Preset{"time travel", -100.f, 500.f, 0, 30, 100, true, -24.f, 0.0f, 0.0f, 24.0f, 0.f, 0.f, -1.f, 1.f, -80.f, 0.f},
